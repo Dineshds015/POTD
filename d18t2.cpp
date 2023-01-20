e865b04f19fc282b0af1845e2f94e27e9a3d572e
Dinesh Suthar <113771324+Dineshds015@users.noreply.github.com>
2023-01-21 00:36:51 +0530

    Create d18t2.cpp

---
+++ d18t2.cpp
@@ -0,0 +1,31 @@
+// 860. Lemonade Change
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five=0,ten=0,twenty=0;
+        for(int i=0;i<bills.size();i++)
+        {
+            if(bills[i]==5)
+                five++;
+            else if(bills[i]==10)
+            {
+                ten++;
+                five--;
+            }
+            else
+            {
+                twenty++;
+                five--;
+                if(ten)
+                    ten--;
+                else
+                    five-=2;
+            }
+            if(five<0 || ten<0 || twenty<0)
+                return 0;
+        }
+        
+        return 1;
+    }
+};
